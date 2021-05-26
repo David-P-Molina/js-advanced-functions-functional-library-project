be9df4fccc568ec3ff3523c8f68f1b09ebfba99e
David Moina <davemolina777@gmail.com>
2021-05-26 15:49:44 -0500

    commit tree 3e63f4a892307da406f421ea53dac6da2a151c40

--- index.js
+++ index.js
@@ -3,22 +3,95 @@ const fi = (function() {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
-
-    each: function() {
-
+//
+    each: function(collection, iteration) {
+      let originalCollection;
+      if (typeof collection === "object") {
+        originalCollection = Object.assign({}, collection)
+        for ( const element in collection ) {
+          iteration(collection[element])
+        } 
+      } else {
+        originalCollection = Array.from(collection)
+        for ( const element of collection ) {
+          iteration(element)
+      }
+    }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArray = []
+      if (Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        newArray.push(callback(collection[i], i, collection))
+      }
+    } else {
+      for ( const property in collection ) {
+        newArray.push(callback(collection[property]))
+      }
+    }
+      return newArray
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, accumulator ) {
+      for ( const element of collection) {
+        if (typeof accumulator === "undefined") {
+          accumulator = collection[0]
+        } else {
+        accumulator = callback(accumulator, element, collection)
+        }
+      }
+      return accumulator
     },
 
+    find: function(collection, predicate) {
+      
+      for (const element of collection) {
+       if (element === predicate) {
+        return element
+       } else {
+         return undefined
+       }
+      }
+    },
+
+    filter: function() {
+
+    },
+    size: function() {
+
+    },
+    first: function() {
+
+    },
+    last: function() {
+
+    },
+    compact: function() {
+
+    },
+    sortBy: function() {
+
+    },
+    flatten: function() {
+
+    },
+    uniq: function() {
+
+    },    
+    keys: function() {
+
+    },
+    values: function() {
+
+    },
     functions: function() {
 
     },
+
+
+
 
 
   }
